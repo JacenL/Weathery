b4e57ce1fa26c7f251542aadf1ae09fda94993e2
JacenL <liangjason17@gmail.com>
2025-03-07 09:28:53 -0500

    Adding line to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,8 @@ export default function About() {
         <div className="ml-5 mt-50">
             <h1 className="text-3xl font-bold mb-4 text-gray-600">About Weathery</h1>
             <p className="text-lg text-gray-500">
-            Weathery is a weather forecasting application that provides accurate and up-to-date weather information.
+            Weathery is a weather forecasting application that provides accurate and up-to-date weather information.<br />
+            This website was built for the 2nd sprint in the Future of Programming Languges course.
             </p>
         </div>
       </div>
