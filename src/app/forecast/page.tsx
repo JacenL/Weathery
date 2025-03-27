e60c351dec5b97ce62d312dc0855bde10912bb47
Jason <liangjason17@gmail.com>
2025-03-26 23:10:15 -0400

    meeting requirements with next

--- src/app/forecast/page.tsx
+++ src/app/forecast/page.tsx
@@ -1,10 +1,13 @@
 import Forecast from "../../components/forecast"
+import { Suspense } from "react";
 
 export default function ForecastPage() {
     return (
       <div className="flex justify-center items-start min-h-screen">
         <div className="mt-10">
-          <Forecast />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Forecast />
+          </Suspense>
         </div>
       </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Weather from "../components/weather";
+import { Suspense } from "react";
 
 export default function Current() {
   return (
     <div className="flex justify-center items-start min-h-screen">
       <div className="mt-10">
-        <Weather />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Weather />
+        </Suspense>
       </div>
     </div>
   );
